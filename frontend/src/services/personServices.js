cbf2f0176a368d8b2e180efb2e9c398ed7cb17ed
Ari Toivanen <ari.toivanen@edu.karelia.fi>
2024-01-02 00:34:02 +0200

    sdfvgbgbb

--- frontend/src/services/personServices.js
+++ frontend/src/services/personServices.js
@@ -2,24 +2,24 @@ import axios from 'axios'
 const baseUrl = '/api/persons'
 
 const getAll = () => {
-    const request = axios.get(baseUrl)
-    return request.then(response => response.data)
+  const request = axios.get(baseUrl)
+  return request.then((response) => response.data)
 }
 
-const create = personObject => {
-    const request = axios.post(baseUrl, personObject)
-    return request.then(response => response.data)
+const create = (personObject) => {
+  const request = axios.post(baseUrl, personObject)
+  return request.then((response) => response.data)
 }
 
 const destroy = (id) => {
-    const request = axios.delete(`${baseUrl}/${id}`)
-    return request.then(response => response.data)
+  const request = axios.delete(`${baseUrl}/${id}`)
+  return request.then((response) => response.data)
 }
 
 const modify = (id, newNumber) => {
-    const request = axios.put(`${baseUrl}/${id}`, newNumber)
-    return request.then(response => response.data)
+  const request = axios.put(`${baseUrl}/${id}`, newNumber)
+  return request.then((response) => response.data)
 }
 
-
+// eslint-disable-next-line import/no-anonymous-default-export
 export default { getAll, create, destroy, modify }
